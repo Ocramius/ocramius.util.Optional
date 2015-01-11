a2d358d65008259dcdee01cd25adf6ed024dac56
Marco Pivetta <ocramius@gmail.com>
2015-01-11 12:27:46 +0100

    First test-case for basic coverage

---
+++ test/ocramius/utilTest/OptionalTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace ocramius\utilTest;
+
+
+use ocramius\util\Optional;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Tests for {@see \ocramius\util\Optional}
+ *
+ * @covers \ocramius\util\Optional
+ */
+class OptionalTest extends PHPUnit_Framework_TestCase
+{
+    public function testNewEmptyAlwaysReturnsSameInstance()
+    {
+        $this->assertSame(Optional::newEmpty(), Optional::newEmpty());
+    }
+}
