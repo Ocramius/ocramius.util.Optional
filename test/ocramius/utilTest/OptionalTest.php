969c8131c9da99371dbf93666d1d39743fab6b17
Marco Pivetta <ocramius@gmail.com>
2015-01-11 12:40:34 +0100

    Testing basic retrieval of an optional from a nullable type

--- test/ocramius/utilTest/OptionalTest.php
+++ test/ocramius/utilTest/OptionalTest.php
@@ -6,6 +6,7 @@ namespace ocramius\utilTest;
 use ocramius\util\exception\NullPointerException;
 use ocramius\util\Optional;
 use PHPUnit_Framework_TestCase;
+use stdClass;
 
 /**
  * Tests for {@see \ocramius\util\Optional}
@@ -24,8 +25,18 @@ class OptionalTest extends PHPUnit_Framework_TestCase
         $this->assertFalse(Optional::newEmpty()->isPresent());
     }
 
-    public function testGettingFromEmptyValueReturnsAnEmptyInstance()
+    public function testCreationFromEmptyValueReturnsAnEmptyInstance()
     {
         $this->assertSame(Optional::newEmpty(), Optional::ofNullable(null));
+    }
+
+    public function testCreationFromNonEmptyValueReturnsNonEmptyInstance()
+    {
+        $value    = new stdClass();
+        $optional = Optional::ofNullable($value);
+
+        $this->assertNotSame(Optional::newEmpty(), $optional);
+
+        $this->assertSame($value, $optional->get());
     }
 }
